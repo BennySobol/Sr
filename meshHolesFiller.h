cdd9d3331bccc547c9e932735e4ed4bbcfd3f782
Alon Negev <alon170603@gmail.com>
2021-02-08 22:59:12 +0200

    added files of mesh Holes Filler

---
+++ meshHolesFiller.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+
+class MeshHolesFiller 
+{
+private:
+
+public:
+
+	MeshHolesFiller(std::string inputFile, std::string saveFile);
+	~MeshHolesFiller() = default;
+
+
+};
